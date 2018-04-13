59ed8a3d94d8980c44f2d28141912a3cc4a28176
blinky3713 <blinky@foam.space>
2018-04-13 14:40:23 -0400

    deploy script

--- src/Main.purs
+++ src/Main.purs
@@ -1,9 +1,50 @@
 module Main where
 
 import Prelude
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = do
-  log "Hello sailor!"
+import Chanterelle (deployMain)
+import Chanterelle.Deploy (deployContract)
+import Chanterelle.Internal.Types (DeployM, DeployConfig(..))
+import ContractConfig (simpleStorageConfig, mockERC20Config, payableTestConfig, simpleErrorTestConfig, complexStorageConfig)
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Console (CONSOLE)
+import Control.Monad.Eff.Exception (EXCEPTION)
+import Control.Monad.Reader.Class (ask)
+import Data.Lens ((?~))
+import Data.Maybe (fromJust)
+import Network.Ethereum.Web3 (Address, ETH, _from, _gas, defaultTransactionOptions)
+import Network.Ethereum.Web3.Types.BigNumber (parseBigNumber, decimal)
+import Node.FS.Aff (FS)
+import Node.Process (PROCESS)
+import Partial.Unsafe (unsafePartial)
+
+main :: forall e. Eff (console :: CONSOLE, eth :: ETH, fs :: FS, process :: PROCESS, exception :: EXCEPTION | e) Unit
+main = deployMain deployScript
+
+
+
+type DeployResults =
+  ( simpleStorage :: Address
+  , mockERC20 :: Address
+  , payableTest :: Address
+  , simpleErrorTest :: Address
+  , complexStorage :: Address
+  )
+
+deployScript :: forall eff. DeployM eff (Record DeployResults)
+deployScript = do
+  deployCfg@(DeployConfig {primaryAccount}) <- ask
+  let bigGasLimit = unsafePartial fromJust $ parseBigNumber decimal "4712388"
+      txOpts = defaultTransactionOptions # _from ?~ primaryAccount
+                                         # _gas ?~ bigGasLimit
+  simpleStorage <- deployContract txOpts simpleStorageConfig
+  mockERC20 <- deployContract txOpts mockERC20Config
+  payableTest <- deployContract txOpts payableTestConfig
+  simpleErrorTest <- deployContract txOpts simpleErrorTestConfig
+  complexStorage <- deployContract txOpts complexStorageConfig
+  pure { simpleStorage: simpleStorage.deployAddress
+       , mockERC20: mockERC20.deployAddress
+       , payableTest: payableTest.deployAddress
+       , simpleErrorTest: simpleErrorTest.deployAddress
+       , complexStorage: complexStorage.deployAddress
+       }
