d9d6586f623e5c210eeca0e5de7758dc04ca284e
martyall <martyall@protonmail.com>
2023-09-15 23:59:26 -0700

    fix ci

--- compile-contracts.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-find ./contracts -name "*.sol" | xargs npx solc --abi -o build
-
-for f in ./build/__contracts_*_sol_*.abi; do
-  target="${f##*_}"
-  target="./build/${target%.abi}.json"
-  mv "$f" "$target"
-done
